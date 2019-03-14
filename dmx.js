cb4ee69fa6690fee818ba7e05eb1ed70a7e15afa
Gerald Wodni <gerald.wodni@gmail.com>
2019-03-15 00:01:02 +0100

    lightshow working

---
+++ dmx.js
@@ -0,0 +1,149 @@
+// xell dmx interface
+// (c)copyright 2019 by Gerald Wodni <gerald.wodni@gmail.com>
+
+const _          = require("underscore");
+const SerialPort = require("serialport");
+
+const dmxChannels = 128;
+const buffer = Buffer.alloc( dmxChannels );
+var pars = [];
+var queue = [];
+
+function setChannel( index, value ) {
+    buffer[ index-1 ] = value;
+    //console.log( "SetChannel", index, value );
+}
+function setPar( index, color ) {
+    for( var i = 0; i < color.length; i++ )
+        setChannel( pars[index] + i, color[i] );
+}
+function setAllPars( color ) {
+    for( var index = 0; index < pars.length; index++ )
+        for( var i = 0; i < color.length; i++ )
+            setChannel( pars[index] + i, color[i] );
+}
+
+var currentAnmation = "init";
+var animationCountdown = 0;
+const animations = {
+    init: {
+        cycles: 0,
+        init: function() {
+            setAllPars( [   0,   0,   0,   0, 255, 0] );
+        },
+        run: function() {
+            setAllPars( [   0,   0,   0,   0, 255, 0] );
+        }
+    },
+    pizzaTime: {
+        cycles: 44*15,
+        init: function() {
+            setPar( 0, [ 255,   0,   0,   0, 0, 0] );
+            setPar( 1, [   0,   0,   0, 255, 0, 0] );
+            setPar( 2, [   0, 255,   0,   0, 0, 0] );
+            setPar( 3, [   0,   0,   0,   0, 0, 0] );
+        }
+    },
+    placeFirst: {
+        cycles: 44*1.5,
+        init: function() {
+            setAllPars( [   0,   0,   0, 255, 0, 0] );
+        },
+        run: function(cycle) {
+            setAllPars( [   0,   0,   0,   0, 0, 0] );
+            setPar( cycle%pars.length, [   0,   0,   0, 255, 0, 0] );
+        }
+    },
+    firstBlood: {
+        cycles: 44*4,
+        init: function() {
+            setAllPars( [   0,   0,   0, 255, 0, 0] );
+        },
+        run: function(cycle) {
+            setAllPars(                [   0,     0,   0,   0, 0, 0] );
+            setPar( (cycle/4)%pars.length, [   255,   0,   0,   0, 0, 0] );
+        }
+    },
+    moodHappy: {
+        cycles: 0,
+        init: function() {
+            setAllPars( [   0, 255,   0,   0, 0, 0] );
+        }
+    },
+    moodNeutral: {
+        cycles: 0,
+        init: function() {
+            setAllPars( [ 255, 255,   0,   0, 0, 0] );
+        }
+    },
+    moodSad: {
+        cycles: 0,
+        init: function() {
+            setAllPars( [ 255,   0,   0,   0, 0, 0] );
+        }
+    }
+};
+
+function playAnimation() {
+    if( animationCountdown == 0 && queue.length > 0 ) {
+        currentAnmation = queue.shift();
+        animations[ currentAnmation ].init();
+        animationCountdown = animations[ currentAnmation ].cycles;
+    }
+    else if( animationCountdown > 0 )
+        animationCountdown--;
+
+    if( _.has( animations[ currentAnmation ], "run" ) )
+        animations[ currentAnmation ].run( animationCountdown );
+}
+
+module.exports = {
+    setup: function( k ) {
+        const port = new SerialPort( k.getWebsiteConfig("serialPort.port"), {
+            baudRate: k.getWebsiteConfig("serialPort.baudRate")
+        })
+
+        buffer.fill( 0 );
+
+        pars = k.getWebsiteConfig("devices.pars");
+
+        console.log( "SERIAL START".bold.yellow );
+        port.on( "error", function( err ) {
+            console.log( "SERIAL ERROR".bold.yellow, err );
+        });
+
+        var mode = "waitSync";
+        var cycle = 0;
+        port.on( "data", function( data ) {
+            //console.log( "GOT SERIAL DATA:".bold.yellow, data );
+
+            if( data[0] == 223 && mode == "waitSync" ) {
+                if( data.length == 1 ) {
+                    mode = "waitCycle";
+                    return;
+                }
+                cycle = data[1];
+            }
+            else if( mode == "waitCycle" ) {
+                cycle = data[0];
+                mode = "waitSync"
+            }
+            
+            playAnimation();
+            port.write( buffer );
+        });
+
+        /* initial write */
+        port.write( buffer );
+    },
+    setChannel: setChannel,
+    setPar: setPar,
+    setAllPars: setAllPars,
+    getParCount() {
+        return pars.length;
+    },
+    queueAnimation(name) {
+        queue.push( name );
+    }
+}
+
--- site.js
+++ site.js
@@ -2,18 +2,18 @@
 // (c)copyright 2019 by Gerald Wodni <gerald.wodni@gmail.com>
 
 const _ = require("underscore");
+const socketIoClient = require('socket.io-client')
 
 module.exports = {
     setup: function( k ) {
-        const parStartAddress = 32;
-        const parCount = 6;
+
+        const pars = k.getWebsiteConfig("devices.pars");
 
         /* value aggregator - keep static for now, extend if needed */
         function vals( req, obj ){
             return _.extend( obj || {}, {
                 title: "w1y - we 1llum1n4t3 y0u",
-                parStartAddress: parStartAddress,
-                parCount: parCount,
+                pars: pars,
                 channelColors: {
                     "r": "#F00",
                     "g": "#0F0",
@@ -29,5 +29,87 @@ module.exports = {
         k.router.get("/", function( req, res ) {
             k.jade.render( req, res, "home", vals(req) );
         });
+
+        const socketIoHost = k.getWebsiteConfig( "dashboard" );
+        const socket = socketIoClient( socketIoHost );
+
+        socket.on('connect', function(){
+            console.log( "SOCKET CONNECTED".bold.magenta );
+        });
+        socket.on('updateServices', function(data){
+            var okCount = 0, failCount = 0;
+            data.forEach( service => {
+                var statusWeight = service.status_max_tick - service.status_min_tick;
+                //console.log( "SERVICE".yellow, service.name.green, service.status, service.up, service.down );
+                if( service.up )
+                    okCount++;
+                if( service.down )
+                    failCount++;
+            });
+
+            var happyness = okCount / (failCount + okCount);
+            console.log( "HAPPYNESS".bold.red, happyness.toFixed(2) );
+
+            if( happyness >0.6 )
+                k.reg("dmx").queueAnimation( "moodHappy" );
+            else if( happyness < 0.3 )
+                k.reg("dmx").queueAnimation( "moodSad" );
+            else
+                k.reg("dmx").queueAnimation( "moodNeutral" );
+        });
+        var lastAlert = 0;
+        socket.on('updateAlerts', function(data){
+            data.forEach( alert => {
+                if( alert.time <= lastAlert )
+                    return;
+                lastAlert = alert.time;
+                console.log( "ALERT".red, alert.message.green, alert.sound, alert.time );
+
+                if( alert.message == "The game has started! Good hunting!" ) {
+                    console.log( "PLAY START".bold.white );
+                }
+                if( alert.message.indexOf( "We exploited" ) == 0 ) {
+                    console.log( "ALERT".red, alert.message.green, alert.sound, alert.time );
+                    console.log( "PLAY FLASH".bold.white );
+                    k.reg("dmx").queueAnimation( "firstBlood" );
+                }
+                if( alert.sound == "pizzatime.ogg" ) {
+                    k.reg("dmx").queueAnimation( "pizzaTime" );
+                    console.log( "PLAY PIZZA".bold.white );
+                }
+            });
+        });
+        var lastGlobalStateTick = 0;
+        var lastRank = 0;
+        socket.on('updateGlobalStates', function( data ){
+            try {
+                var state = data[0];
+                console.log( "UPDA GLOB".bold.magenta, state.tick );
+                if( state.tick <= lastGlobalStateTick )
+                    return;
+                lastGlobalStateTick = state.tick;
+                rank = state.score.rank;
+
+                console.log( "RANK".bold.magenta, rank );
+
+                if( rank <= 3 && rank < lastRank ) {
+                    k.reg("dmx").queueAnimation( "placeFirst" );
+                }
+                lastRank = rank;
+            }
+            catch( x ) {
+            }
+        });
+        var rank = -1;
+        socket.on('updateConfig', function( data ){
+            console.log( "CONFIG".bold.magenta, data );
+        });
+        socket.on('disconnect', function(){
+            console.log( "SOCKET DISCONNECTED".bold.magenta );
+        });
+
+        /* dmx interface */
+        k.useSiteModule( null, k.website, "dmx.js", { register: "dmx" } );
+        //k.reg("dmx").setAllPars( [0, 0, 0, 0, 255, 0 ] );
     }
 };
